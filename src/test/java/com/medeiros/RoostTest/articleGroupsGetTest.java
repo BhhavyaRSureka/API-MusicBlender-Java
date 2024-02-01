f53e1a95870b8433837bfaa3f9eb242cf6265661
BhhavyaRSureka <42551911+BhhavyaRSureka@users.noreply.github.com>
2024-02-01 18:20:29 +0530

    Error Fixes

--- src/test/java/com/medeiros/RoostTest/articleGroupsGetTest.java
+++ src/test/java/com/medeiros/RoostTest/articleGroupsGetTest.java
@@ -52,11 +52,7 @@ public class articleGroupsGetTest {
                 
   
                 Response response = given()
-				.pathParam("company", map.get("company") != null ? map.get("company") : "")
-				.pathParam("from", map.get("from") != null ? map.get("from") : "")
-				.pathParam("offset", map.get("offset") != null ? map.get("offset") : "")
-				.pathParam("limit", map.get("limit") != null ? map.get("limit") : "")
-				.pathParam("merchant", map.get("merchant") != null ? map.get("merchant") : "")
+				.queryParam("company", map.get("company") != null ? map.get("company") : "")
 				.header("Authorization", "Bearer " + System.getenv("AUTH_TOKEN"))
                 .when()
                 .get("/api/customer/articleGroups")  
